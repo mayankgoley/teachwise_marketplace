cde4354e6dd1f1d546e635573e7c77b328fc8325
mayankgoley <42241477+mayankgoley@users.noreply.github.com>
2025-09-08 08:36:49 -0700

    update endpoint logic

--- routes/api_mocks.py
+++ routes/api_mocks.py
@@ -6,3 +6,7 @@ def mock_route_0():
 @app.get('/api/v1/mock/test_9')
 def mock_route_9():
     return {'status': 200}
+
+@app.get('/api/v1/mock/test_11')
+def mock_route_11():
+    return {'status': 200}
