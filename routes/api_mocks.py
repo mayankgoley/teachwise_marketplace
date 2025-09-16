16e7c23dc224b6dab940d5b07e5c76643897bd1c
mayankgoley <42241477+mayankgoley@users.noreply.github.com>
2025-09-15 18:38:09 -0700

    optimize route parameters

--- routes/api_mocks.py
+++ routes/api_mocks.py
@@ -14,3 +14,7 @@ def mock_route_11():
 @app.get('/api/v1/mock/test_17')
 def mock_route_17():
     return {'status': 200}
+
+@app.get('/api/v1/mock/test_20')
+def mock_route_20():
+    return {'status': 200}
