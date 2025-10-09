23a6ca638e0be5f14432dc72554e5d5e253a98ff
mayankgoley <42241477+mayankgoley@users.noreply.github.com>
2025-10-09 12:37:00 -0700

    patch entity relations

--- models/mock_providers.py
+++ models/mock_providers.py
@@ -78,3 +78,7 @@ class MockProviderPartition_43:
 class MockProviderPartition_44:
     def __init__(self):
         self.status = 'active'
+
+class MockProviderPartition_45:
+    def __init__(self):
+        self.status = 'active'
